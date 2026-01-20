7b459ec0e86832a5284b9f667b38850de273a6a4
Sergio de la Fuente <sergio4vientos@gmail.com>
2026-01-20 20:33:28 +0100

    Preparar lógica del formulario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption, IonInput, IonButton } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption, IonInput, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonSelect, IonSelectOption, IonInput, IonButton, FormsModule],
 })
 export class HomePage {
+
+  locationType: string = '';
+  cityName: string = '';
+
   constructor() {}
+
+  isFormValid(): boolean {
+    if (!this.locationType) {
+      return false;
+    }
+
+    if (this.locationType === 'city' && !this.cityName.trim()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getCurrentWeather() {
+  }
+
+  getForecast() {
+  }
 }
